e59b2f8517d126308b1c5c766c141278371a40f0
Julia Jeleńska <125867800+Jelon455@users.noreply.github.com>
2024-09-01 22:14:06 +0200

    Start the project

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 01.09.2024 22:12:48
+ * Author : Julia
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
